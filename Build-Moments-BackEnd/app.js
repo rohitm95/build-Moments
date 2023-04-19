fc7451916da4372c66f21789a95a08a6e26dee9b
Rohit Mamidwar <rohitm95@gmail.com>
2023-04-19 17:57:17 +0530

    Modified temporary commit code

--- Build-Moments-BackEnd/app.js
+++ Build-Moments-BackEnd/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require("path");
 
 const express = require("express");
@@ -7,15 +8,11 @@ const multer = require("multer");
 const cors = require("cors");
 var fs = require("fs");
 var morgan = require("morgan");
-const aws = require("aws-sdk"),
-  { S3 } = require("@aws-sdk/client-s3");
-const multerS3 = require("multer-s3");
 
 const feedRoutes = require("./routes/feed");
 const authRoutes = require("./routes/auth");
 
-const MONGODB_URI =
-  "mongodb+srv://rohitm95:jbMS2zI5m6umJ8Vr@cluster0.zow71as.mongodb.net/moments";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
@@ -79,48 +76,7 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-const config = {
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY | "AKIA5ANNYP7DZ4HP6T6C",
-    secretAccessKey:
-      process.env.AWS_SECRET_ACCESS_KEY |
-      "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",
-  },
-};
-
-const s3 = new S3(config);
-
-app.get("/generatepresignedurl", function (req, res) {
-  var fileurls = [];
-
-  const params = {
-    Bucket: "mymomentsbucket",
-    Key: req.query.fileName,
-    Expires: 40 * 60, // Time until presigned URL is valid
-    ACL: "public-read",
-    ContentType: req.query.fileType,
-  };
-
-  s3.getSignedUrl("putObject", params, function async(err, url) {
-    if (err) {
-      res.json({
-        success: false,
-        message: "Pre- Signed URL error",
-        urls: fileurls,
-      });
-    } else {
-      fileurls[0] = url;
-      res.json({
-        success: true,
-        message: "AWS SDK S3 Pre- signed urls generated successfully",
-        urls: fileurls,
-      });
-    }
-  });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
--- Build-Moments-FrontEnd/src/app/moments/add-moment/add-moment.component.ts
+++ Build-Moments-FrontEnd/src/app/moments/add-moment/add-moment.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-import { HttpEvent, HttpEventType } from '@angular/common/http';
-import { FileUploadService } from 'src/app/shared/file-upload.service';
 
 @Component({
   selector: 'app-add-moment',
@@ -11,104 +9,47 @@ import { FileUploadService } from 'src/app/shared/file-upload.service';
   styleUrls: ['./add-moment.component.scss'],
 })
 export class AddMomentComponent implements OnInit {
-  fileObj: File;
-  fileUrl: string;
-  errorMsg: boolean;
-  visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
   readonly seperatorKeysCodes: number[] = [ENTER, COMMA];
-  form: FormGroup;
-  progress: number = 0;
+  formData: FormGroup;
   constructor(
     public fb: FormBuilder,
-    public fileUploadService: FileUploadService
   ) {
-    this.form = this.fb.group({
-      tags: this.fb.array([], Validators.required),
-      name: [''],
-      avatar: [null],
-    });
-    this.errorMsg = false;
   }
 
-  ngOnInit() {}
-
-  get tagControls(): FormArray {
-    return this.form.controls['tags'] as FormArray;
+  ngOnInit(): void {
+    this.initForm();
+    this.ppssToDisplay();
   }
 
-  addTag(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
+  initForm(): void {
+    this.formData = this.fb.group({
+      requirements: this.fb.array([]),
+    })
+  }
 
-    // Add a tag
+  ppssToDisplay(): void {
+
+  }
+
+  add(e: MatChipInputEvent) {
+    const input = e.input;
+    const value = e.value;
+
     if ((value || '').trim()) {
-      this.tagControls.push(this.fb.control(value));
+      const control = <FormArray>this.formData.controls['requirements'];
+      control.push(this.fb.control(value.trim()))
     }
 
-    // Reset the input value
     if (input) {
       input.value = '';
     }
   }
 
-  removeTag(tag: string): void {
-    const index = this.tagControls.value.indexOf(tag);
-    if (index >= 0) {
-      this.tagControls.removeAt(index);
-    }
+  remove(i: number) {
+    let control = <FormArray>this.formData.controls['requirements'];
+    control.removeAt(i);
   }
-
-  onFilePicked(event: Event): void {
-    const { target } = event;
-    this.errorMsg = false;
-    console.log('target', target);
-    // const FILE = target.files[0];
-    // this.fileObj = FILE;
-  }
-
-  onFileUpload() {
-    if (!this.fileObj) {
-      this.errorMsg = true;
-      return;
-    }
-    this.fileUploadService
-      .getpresignedurls(this.fileObj.name, this.fileObj.type)
-      .subscribe((res) => {
-        if (res.success) {
-          const fileuploadurl = res.urls[0];
-          const imageForm = new FormData();
-          imageForm.append('file', this.fileObj);
-          this.fileUploadService
-            .uploadfileAWSS3(fileuploadurl, this.fileObj.type, this.fileObj)
-            .subscribe((event) => {});
-        }
-      });
-  }
-
-  // submitUser() {
-  //   this.fileUploadService
-  //     .addUser(this.form.value.name, this.form.value.avatar)
-  //     .subscribe((event: HttpEvent<any>) => {
-  //       switch (event.type) {
-  //         case HttpEventType.Sent:
-  //           console.log('Request has been made!');
-  //           break;
-  //         case HttpEventType.ResponseHeader:
-  //           console.log('Response header has been received!');
-  //           break;
-  //         case HttpEventType.UploadProgress:
-  //           // this.progress = Math.round(event.loaded / event.total * 100);
-  //           console.log(`Uploaded! ${this.progress}%`);
-  //           break;
-  //         case HttpEventType.Response:
-  //           console.log('User successfully created!', event.body);
-  //           setTimeout(() => {
-  //             this.progress = 0;
-  //           }, 1500);
-  //       }
-  //     });
-  // }
 }
