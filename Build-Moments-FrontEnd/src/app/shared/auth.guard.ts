790682530cd5d577f51034722c3b4b504b6e4fa5
Rohit Mamidwar <rohitm95@gmail.com>
2023-06-29 09:09:53 +0530

    Updated @angular/core to v16

--- Build-Moments-FrontEnd/src/app/shared/auth.guard.ts
+++ Build-Moments-FrontEnd/src/app/shared/auth.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router, private toastr: ToastrService) {}
   canActivate(
     route: ActivatedRouteSnapshot,
